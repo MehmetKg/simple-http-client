b67a589e64c39e821660f14d45cae2231b2bed47
Mehmet Karagülle <117034396+MehmetKg@users.noreply.github.com>
2025-04-29 21:18:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((target_host, target_port))
+
+request = "GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n"
+client.send(request.encode('utf-8'))
+
+response = b""
+while True:
+    part = client.recv(4096)
+    if not part:
+        break
+    response += part
+
+print(response.decode('utf-8', errors='replace'))
